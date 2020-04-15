8c84179fdf1f8ae836c2ecebd802c267fce0369a
Humayun Ahmed <humayun2911@gmail.com>
2020-04-15 11:42:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,3 @@
 def display_board
-<<<<<<< HEAD
   print "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n"
-=======
-  puts "Welcome to Tic Tac Toe"
-  print "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   "
->>>>>>> 55adda233d75ec346b99345c2528cbb194349dd0
 end
